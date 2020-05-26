f6a8047f4019bdabe48000ed8e6b825b8fa33519
Andreas Panayiotou <andrewpany2002@gmail.com>
2020-05-26 09:46:52 +0100

    Includes all changes up to section 8-110.

---
+++ Section 7 - Components/Assignment 6/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <app-header></app-header>
+    <hr>
+    <div class="row">
+      <app-server-status></app-server-status>
+      <app-server-details></app-server-details>
+    </div>
+    <hr>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+  import Header from "./components/Shared/Header";
+  import Footer from "./components/Shared/Footer";
+  import ServerStatus from "./components/Server/ServerStatus";
+  import ServerDetails from "./components/Server/ServerDetails";
+
+  export default {
+    components: {
+      appHeader : Header,
+      'app-footer' : Footer,
+      'app-server-status': ServerStatus,
+      'app-server-details': ServerDetails
+    }
+  }
+</script>
+
+<style>
+
+</style>
