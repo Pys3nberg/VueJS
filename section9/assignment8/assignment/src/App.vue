9c8a7fd0e436ea7cbbcd55f47f86558803f1f207
Andreas Panayiotou <andrewpany2002@gmail.com>
2020-05-29 13:36:21 +0100

    Includes changes up to section 9.

---
+++ section9/assignment8/assignment/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <br>
+            <div class="row">
+              <div class="col-sm-4">
+                <button class="btn btn-danger pr-lg-2"
+                        @click="selectedComponent='appRed'">Load Red Component</button>
+              </div>
+              <div class="col-sm-4">
+                <button class="btn btn-success"
+                        @click="selectedComponent='appGreen'">Load Green Component</button>
+              </div>
+              <div class="col-sm-4">
+                <button class="btn btn-primary"
+                        @click="selectedComponent='appBlue'">Load Blue Component</button>
+              </div>
+            </div>
+          <br>
+          <component :is="selectedComponent">
+            <p>{{content}}</p>
+          </component>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  import Red from "./components/Red";
+  import Green from "./components/Green";
+  import Blue from "./components/Blue";
+
+  export default {
+    components:{
+      appRed: Red,
+      appGreen: Green,
+      appBlue: Blue
+    },
+    data(){
+      return {
+        selectedComponent: 'appRed',
+        content: "This is some content"
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ section9/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <button @click="selectedComponent='appQuote'">Quote</button>
+          <button @click="selectedComponent='appAuthor'">Author</button>
+          <button @click="selectedComponent='appNew'">New</button>
+          <hr>
+          <p>{{selectedComponent}}</p>
+          <keep-alive>
+            <component :is="selectedComponent">
+              <p>Default Content</p>
+            </component>
+          </keep-alive>
+<!--          <app-quote>-->
+<!--            <h2 slot="title">{{quoteTitle}}</h2>-->
+<!--            <p slot="quote">A wonderful Quote!</p>-->
+<!--          </app-quote>-->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Quote from "./components/Quote";
+import Author from "./components/Author";
+import New from "./components/New";
+
+export default {
+  components: {
+    appQuote: Quote,
+    appAuthor: Author,
+    appNew: New
+  },
+  data() {
+    return {
+      quoteTitle: 'The Quote',
+      selectedComponent: 'appQuote'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
