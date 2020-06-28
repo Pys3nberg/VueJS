dee35290fe8197135431e1deeada280071fcb276
Andreas Panayiotou <andrewpany2002@gmail.com>
2020-06-28 15:41:30 +0100

    Course complete!

--- section22-axios/src/components/auth/signup.vue
+++ section22-axios/src/components/auth/signup.vue
@@ -8,30 +8,34 @@
                   type="email"
                   id="email"
                   v-model="email"
-                  @input="$v.email.$touch()">
+                  @blur="$v.email.$touch()">
           <p v-if="!$v.email.email">Please provide a valid email address</p>
           <p v-if="!$v.email.required">Field must not be empty</p>
 
         </div>
-        <div class="input">
+        <div class="input"  :class="{invalid: $v.age.$error}">
           <label for="age">Your Age</label>
           <input
                   type="number"
                   id="age"
+                  @blur="$v.age.$touch()"
                   v-model.number="age">
+          <p v-if="!$v.age.minVal">You have to be atleast {{ $v.age.$params.minVal.min }} years old.</p>
         </div>
-        <div class="input">
+        <div class="input" :class="{invalid:$v.password.$error}">
           <label for="password">Password</label>
           <input
                   type="password"
                   id="password"
+                  @blur="$v.password.$touch()"
                   v-model="password">
         </div>
-        <div class="input">
+        <div class="input" :class="{invalid:$v.confirmPassword.$error}">
           <label for="confirm-password">Confirm Password</label>
           <input
                   type="password"
                   id="confirm-password"
+                  @blur="$v.confirmPassword.$touch()"
                   v-model="confirmPassword">
         </div>
         <div class="input">
@@ -49,23 +53,31 @@
           <div class="hobby-list">
             <div
                     class="input"
+                    :class="{invalid: $v.hobbyInputs.$each[index].$error}"
                     v-for="(hobbyInput, index) in hobbyInputs"
                     :key="hobbyInput.id">
               <label :for="hobbyInput.id">Hobby #{{ index }}</label>
               <input
                       type="text"
                       :id="hobbyInput.id"
-                      v-model="hobbyInput.value">
+                      @blur="$v.hobbyInputs.$each[index].value.$touch()"
+                      v-model="hobbyInputs.value">
               <button @click="onDeleteHobby(hobbyInput.id)" type="button">X</button>
             </div>
+            <p v-if="!$v.hobbyInputs.minLen">You have to specify at least {{ $v.hobbyInputs.$params.minLen.min }} hobbies</p>
+            <p v-if="!$v.hobbyInputs.required">Please add hobbies.</p>
           </div>
         </div>
-        <div class="input inline">
-          <input type="checkbox" id="terms" v-model="terms">
+        <div class="input inline" :class="{invalid: $v.terms.$error}">
+          <input
+                  type="checkbox"
+                  id="terms"
+                  @change="$v.terms.$touch()"
+                  v-model="terms">
           <label for="terms">Accept Terms of Use</label>
         </div>
         <div class="submit">
-          <button type="submit">Submit</button>
+          <button type="submit" :disabled="$v.$invalid">Submit</button>
         </div>
       </form>
     </div>
@@ -73,7 +85,8 @@
 </template>
 
 <script>
-  import { required, email } from 'vuelidate/lib/validators'
+  import axios from 'axios'
+  import { required, email, numeric, minValue, minLength, sameAs, requiredUnless } from 'vuelidate/lib/validators'
   export default {
     data () {
       return {
@@ -90,7 +103,46 @@
       //bind to the data props that are v-bind to the control
       email: {
         required: required,
-        email:email
+        email:email,
+        unique: value => {
+          if(value === '') return true // this validator doesnt care if it is empty
+          return axios.get('/users.json?orderBy="email"&equalTo="'+ value + '"')
+                  .then(response => {
+                    return Object.keys(response.data).length === 0;
+                  })
+        }
+      },
+      age: {
+        required,
+        numeric,
+        minVal: minValue(18)
+      },
+      password: {
+        required,
+        minLen: minLength(6),
+
+      },
+      confirmPassword: {
+        // sameAs: sameAs('password')
+        // this method is useful if you need to check against something special
+        sameAs: sameAs(function(vueInstance){
+          return vueInstance.password;
+        })
+      },
+      terms: {
+        required: requiredUnless(vi=>{
+          return vi.country == 'germany';
+        })
+      },
+      hobbyInputs: {
+        required,
+        minLen: minLength(1),
+        $each: {
+          value: {
+            required,
+            minLen: minLength(5)
+          }
+        }
       }
     },
     methods: {
---
+++ vue-cli-new/Hello2.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Hello2</h1>
+</template>
+
+<script>
+    export default {
+        name: "Hello2"
+    }
+</script>
+
+<style scoped>
+
+</style>
